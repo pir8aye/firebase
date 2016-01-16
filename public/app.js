55d2e335e80e6cc2eed0250738c34b275f132188
Jesse <jesse.heaslip@gmail.com>
2016-01-16 10:48:40 -0800

    adding comments and remove

--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
 'use strict';
 
-var ref = new Firebase("https://<your_fb_url>.firebaseio.com/");
+// var ref = new Firebase("https://<your_fb_url>.firebaseio.com/");
+var ref = new Firebase('https://ch-twitter.firebaseio.com/')
 
-// everytime something is added to ref we append it to the dom
+// everytime a child is added to ref we append it to the dom
+// Possible 'on' refrerences: https://www.firebase.com/docs/web/api/query/on.html
+// snap.val() is the object stored at that location
+// snap.key() is the id of the object
 ref.on('child_added', function(snap){
   $('#tweets').prepend(snap.val().text)
+})
+
+// remove everything at that ref
+$(function () {
+   $('#remove').on('click', function(){
+     ref.remove()
+   })
 })
--- server.js
+++ server.js
@@ -4,26 +4,35 @@ var express       = require('express');
 var app           = express();
 
 var Firebase      = require('firebase')
-var ref           = new Firebase('https://<your_fb_url>.firebaseio.com/')
+// var ref           = new Firebase('https://<your_fb_url>.firebaseio.com/')
+var ref           = new Firebase('https://ch-twitter.firebaseio.com/')
 
 var Twitter       = require('twitter')
 
-var client = new Twitter({
-  consumer_key: CONSUMER_KEY,
-  consumer_secret: CONSUMER_SECRET,
-  access_token_key: TOKEN_KEY,
-  access_token_secret: TOKEN_SECRET
+var client      = new Twitter({
+  // make sure keys are in quotes
+  // do not commit to github with your keys included
+  consumer_key: 'Se1a1quNUBALSX65ciTciFRIq',
+  consumer_secret: 'mhDBcgHB4Wfk9ZIhOztgYJ3YlBmGy9p4qe7JgQyHUf7LLfBfvD',
+  access_token_key: '22101689-hUWKsfP9Ia6VpMNOIGL6nKM7njZorsQOrcBzF7cYu',
+  access_token_secret: 'BeHouu1hxVrfIsvXFDtpdgAbdw3p8782hgfRsQr6AdxKI'
 })
 
+// tells express where to look for front-end files
 app.use(express.static('public'));
 
+// send our index file when the route is hit
 app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
 
-client.stream('statuses/filter', {track: 'nfl'}, function(stream) {
+// stream da tweets! More info: https://dev.twitter.com/streaming/overview
+client.stream('statuses/filter', {track: 'san francisco'}, function(stream) {
   stream.on('data', function(tweet) {
-    // set a unique id!! Adding tweet text to firebase
+    console.log(tweet.text)
+
+    // Unique id!! In this case we are using Date.now(), Adding tweet text to firebase
+    // learn more about saving data to firebase: https://www.firebase.com/docs/web/guide/saving-data.html
     ref.child(Date.now()).set({text: tweet.text})
   })
 })
