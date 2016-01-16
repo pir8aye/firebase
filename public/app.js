0b8862fdd55029ac579340684fd1b827feefbd5b
Jesse <jesse.heaslip@gmail.com>
2016-01-16 11:32:29 -0800

    adding comments

--- public/app.js
+++ public/app.js
@@ -1,19 +1,28 @@
 'use strict';
 
-var ref = new Firebase("https://<your_fb_url>.firebaseio.com/");
+// var ref = new Firebase("https://<your_fb_url>.firebaseio.com/");
+var ref = new Firebase("https://ch-twitter.firebaseio.com/");
 
 // everytime 'child_added' to ref we prepend to dom
 // Possible 'on' refrerences: https://www.firebase.com/docs/web/api/query/on.html
 // snap.val() is the object stored at that location
+// in this case it looks like:
+//  id {
+//    text: tweet.tet
+//  }
 // snap.val().text will get us just the text
 // snap.key() is the id of the object
+
 ref.on('child_added', function(snap){
-  $('#tweets').prepend(snap.val().text)
+
+  $('#tweets').prepend(snap.val().text + ' <br><br>')
+
+  // console.log(snap.val().text)
 })
 
 // remove everything at our ref
 $(function () {
-   $('#remove').on('click', function(){
-     ref.remove()
-   })
+  $('#remove').on('click', function(){
+    ref.remove()
+  })
 })
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express       = require('express');
 var app           = express();
 
 var Firebase      = require('firebase')
-var ref           = new Firebase('https://<your_fb_url>.firebaseio.com/')
+// var ref           = new Firebase('https://<your_fb_url>.firebaseio.com/')
+var ref           = new Firebase("https://ch-twitter.firebaseio.com/");
 
 var Twitter       = require('twitter')
 
@@ -17,15 +18,8 @@ var client      = new Twitter({
   access_token_secret: 'TOKEN_SECRET'
 })
 
-// tells express where to look for front-end files
-app.use(express.static('public'));
-
-// send our index file when the route is hit
-app.get('/', function (req, res) {
-  res.sendfile('index.html');
-});
-
 // stream da tweets! Everything you can stream: https://dev.twitter.com/streaming/overview
+// check out the npm package at: https://www.npmjs.com/package/twitter
 client.stream('statuses/filter', {track: 'san francisco'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text)
@@ -38,5 +32,16 @@ client.stream('statuses/filter', {track: 'san francisco'}, function(stream) {
   })
 })
 
+// tells express where to look for front-end files
+// this will all be covered in a week or two
+app.use(express.static('public'));
+
+// send our index file when the route is hit
+// this will also be covered in a week or two
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+// launch the server on the port shown
 app.listen(3000);
 console.log('listening on 3000');
