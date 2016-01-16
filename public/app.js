071d5552eed8b31d0d38c2500453bebed53c7e3d
Jesse <jesse.heaslip@gmail.com>
2016-01-16 10:05:54 -0800

    adding comments

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@
 
 var ref = new Firebase("https://<your_fb_url>.firebaseio.com/");
 
+// everytime something is added to ref we append it to the dom
 ref.on('child_added', function(snap){
-  $('.tweets').prepend(snap.val().text)
+  $('#tweets').prepend(snap.val().text)
 })
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 'use strict'
 
-require('dotenv').load('.env')
-
 var express       = require('express');
 var app           = express();
 
@@ -9,11 +7,12 @@ var Firebase      = require('firebase')
 var ref           = new Firebase('https://<your_fb_url>.firebaseio.com/')
 
 var Twitter       = require('twitter')
+
 var client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.TOKEN_KEY,
-  access_token_secret: process.env.TOKEN_SECRET
+  consumer_key: CONSUMER_KEY,
+  consumer_secret: CONSUMER_SECRET,
+  access_token_key: TOKEN_KEY,
+  access_token_secret: TOKEN_SECRET
 })
 
 app.use(express.static('public'));
@@ -24,7 +23,7 @@ app.get('/', function (req, res) {
 
 client.stream('statuses/filter', {track: 'nfl'}, function(stream) {
   stream.on('data', function(tweet) {
-    console.log(tweet)
+    // set a unique id!! Adding tweet text to firebase
     ref.child(Date.now()).set({text: tweet.text})
   })
 })
